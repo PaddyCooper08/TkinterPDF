648540ea417e8c38901e6c7c4b73b7151f10e432
PaddyCooper08 <1210@rgsg.co.uk>
2021-05-16 13:41:34 +0100

    Setting up logo and static folder

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ from PIL import Image, ImageTk
 
 
 root = tk.Tk()
-
+canvas = tk.Canvas(root, width=600, height=300)
+canvas.grid(columnspan=3)
+#logo
+logo = Image.open('Static/logo.png')
+logo = ImageTk.PhotoImage(logo)
+logo_label = tk.Label(image=logo)
+logo_label.image = logo
+logo_label.grid(column=1, row=0)
 
 
 
