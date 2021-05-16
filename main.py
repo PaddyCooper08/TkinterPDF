90673152ab73142ba709e7ed47b8ee98e20b65b5
PaddyCooper08 <1210@rgsg.co.uk>
2021-05-16 18:52:51 +0100

    Removed extra spaces in the PDF

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import PyPDF2
 from PIL import Image, ImageTk
 from tkinter.filedialog import askopenfile
+import re
 
 
 root = tk.Tk()
@@ -26,12 +27,14 @@ def open_file():
 		read_pdf = PyPDF2.PdfFileReader(file)
 		page = read_pdf.getPage(0)
 		page_content = page.extractText()
-		print(page_content)
+		
+		output = ' '.join(page_content.split())
+	
 
 
 		#text box
 		text_box = tk.Text(root, height=10, width=50, padx=10, pady=15, font="Garamond" )
-		text_box.insert(1.0, page_content)
+		text_box.insert(1.0, output)
 		text_box.tag_configure("center", justify="center")
 		text_box.tag_add("center", 1.0, "end")
 		text_box.grid(column=1, row=3)
@@ -46,7 +49,6 @@ browse_btn = tk.Button(root, textvariable=browse_text, font="Garamond", bg="#20b
 browse_btn.grid(column=1, row=2)
 canvas = tk.Canvas(root, width=600, height=250)
 canvas.grid(columnspan=3)
-
 
 
 
