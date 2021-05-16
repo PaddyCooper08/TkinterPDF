5fa068f48a29a6733ccdf1f2af03771f9c87b8bb
PaddyCooper08 <1210@rgsg.co.uk>
2021-05-16 13:50:03 +0100

    Adding button

--- main.py
+++ main.py
@@ -13,7 +13,15 @@ logo_label = tk.Label(image=logo)
 logo_label.image = logo
 logo_label.grid(column=1, row=0)
 
+#instructions
+instructions = tk.Label(root, text="Select a PDF file on your computer to extract all it's text", font="Garamond")
+instructions.grid(columnspan=3, column=0, row=1)
 
+#browse button
+browse_text = tk.StringVar()
+browse_text.set('Browse')
+browse_btn = tk.Button(root, textvariable=browse_text, font="Garamond")
+browse_btn.grid(column=1, row=2)
 
 
 
